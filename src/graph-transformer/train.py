e258840b28e171116a539d7872ed52731a605168
anonymousoxford2024 <anonymousoxford2024@gmail.com>
2024-04-15 10:47:59 +0100

    add argparser and linformer to training

--- src/graph-transformer/train.py
+++ src/graph-transformer/train.py
@@ -1,13 +1,23 @@
+import argparse
+
 import torch
 import torch.nn.functional as F
 from torch import nn, optim
+from torch_geometric.data.data import Data
 from torch_geometric.datasets import Planetoid
 
 from evaluation import compute_auroc
+from models.linformer_attention import LinformerGraphTransformer
 from models.full_attention import FullAttentionGraphTransformer
 
 
-def train(model, data, optimizer, epochs=40, patience=5):
+def train(
+    model: nn.Module,
+    data: Data,
+    optimizer: optim.Optimizer,
+    epochs: int = 40,
+    patience: int = 5,
+):
     train_mask = data.train_mask
     val_mask = data.val_mask
 
@@ -48,7 +58,7 @@ def train(model, data, optimizer, epochs=40, patience=5):
     return best_model if best_model is not None else model
 
 
-def evaluate(model: nn.Module, data, mask):
+def evaluate(model: nn.Module, data: Data, mask: torch.Tensor):
     model.eval()
     with torch.no_grad():
         logits = model(data)
@@ -63,29 +73,69 @@ if __name__ == "__main__":
     # data = dataset[0]
     # print(data)
 
-    root = "/tmp/Citeseer"
-    dataset = Planetoid(root=root, name="CiteSeer")
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--model_type",
+        type=str,
+        default="full-attention",
+        choices=["full-attention", "linformer"],
+        help="Type of model to train ('full-attention' or 'linformer').",
+    )
+    parser.add_argument(
+        "--dataset",
+        type=str,
+        default="Citeseer",
+        choices=["Citeseer", "Cora", "Pubmed"],
+        help="Dataset to train models on.",
+    )
+    args = parser.parse_args()
+    model_type = args.model_type
+    dataset_name = args.dataset
+
+    root = f"/tmp/{dataset_name}"
+    dataset = Planetoid(root=root, name=dataset_name)
     data = dataset[0]
+
+    print(f"model_type = {model_type}")
+    print(f"dataset = {dataset}")
+    print(f"data.num_nodes = {data.num_nodes}")
 
     for hidden_dim in [256]:
         for lr in [1e-3]:
             for weight_decay in [0.0]:
-                for _ in range(10):
-                    print(
-                        f"\nhidden_dim = {hidden_dim}\t\tlr = {lr}\t\tweight_decay = {weight_decay}"
-                    )
+                # for _ in range(10):
+                print(
+                    f"\nhidden_dim = {hidden_dim}\t\tlr = {lr}\t\tweight_decay = {weight_decay}"
+                )
+                if model_type == "full-attention":
                     model = FullAttentionGraphTransformer(
                         input_dim=dataset.num_node_features,
                         hidden_dim=hidden_dim,
                         output_dim=dataset.num_classes,
                         num_layers=1,
                     )
+                elif model_type == "linformer":
+                    # Example configuration and model initialization
+                    input_dim = dataset.num_node_features
+                    hidden_dim = hidden_dim
+                    output_dim = dataset.num_classes
+                    num_layers = 1
+                    projection_dim = 64
 
-                    optimizer = optim.Adam(
-                        model.parameters(), lr=lr, weight_decay=weight_decay
+                    model = LinformerGraphTransformer(
+                        input_dim=input_dim,
+                        hidden_dim=hidden_dim,
+                        output_dim=output_dim,
+                        num_layers=num_layers,
+                        projection_dim=64,
+                        n_nodes=data.num_nodes,
                     )
 
-                    out = model(data)
-                    targets = data.y
+                optimizer = optim.Adam(
+                    model.parameters(), lr=lr, weight_decay=weight_decay
+                )
 
-                    trained_model = train(model, data, optimizer, epochs=120)
+                out = model(data)
+                targets = data.y
+
+                trained_model = train(model, data, optimizer, epochs=120)
